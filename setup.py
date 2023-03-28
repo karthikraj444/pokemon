a912ac5bf44865bf07c83fdc3bddafe4b3f42932
Karthik Raj <Karthik.Raj@course5i.com>
2023-03-28 16:56:05 +0530

    recent

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='mlops project to deploy in cloud',
+    author='C5',
+    license='MIT',
+)
